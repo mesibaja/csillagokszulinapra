d45ba45fd1fcdbc9c72ab0a09b6fb999eb51152f
mesibaja <petra.lehoczky@gmail.com>
2017-02-21 20:00:09 +0100

    Fading text

---
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // http://stackoverflow.com/questions/2868047/fullscreen-activity-in-android
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
